e58bbf42326cdd3311847be5292421a5bfd6a40c
watthanai_c <notovel2@gmail.com>
2019-11-17 22:25:38 +0700

    fix cors origin

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const googleAPIKey = "AIzaSyDeFMOZC8QoCqAJF_xlxNguqDiTFzl0gGI"
 const port = process.env.PORT || 8088
-const origin = 'https://scg-test-fe.herokuapp.com/'
+const origin = 'https://scg-test-fe.herokuapp.com'
 const lineConfig = {
     channelAccessToken: 'PqUy1xjYG7Q0NUS7HEqK7+4KgfgonKDanoVa5tbjMEk0PiTTEYqjbzkXrXCv4svqd4KwVjE0o7r7nElQKSTPTd05kx8J44ETYYyqvZPGmxJbd1JwLPvPDEnfs/40LAp9TGIxUuYc9PCfUuNNGjUjagdB04t89/1O/w1cDnyilFU=',
     channelSecret: 'ee0f16e7de175d3087409b2d67ec7abd'
