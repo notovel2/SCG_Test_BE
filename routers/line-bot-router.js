e8c2b3c05aa1657cda5b72257b72f9ba59e09707
watthanai_c <notovel2@gmail.com>
2019-11-17 22:29:34 +0700

    fix bot router

--- routers/line-bot-router.js
+++ routers/line-bot-router.js
@@ -2,15 +2,15 @@ const express = require('express')
 const router = express.Router()
 const {middleware} = require('@line/bot-sdk')
 const {lineConfig} = require('../config')
-router.use(middleware(lineConfig))
 
 router.get('/', (req, res) => {
     res.send('GET')
 })
 
-router.post('/', (req, res) => {
+router.post('/', middleware(lineConfig), (req, res) => {
     console.log('req', req);
-    res.json(req.body.events)
+    res.json({})
+    // res.json(req.body.events)
 })
 
 module.exports = router
