d678d49520ad01e65c6f5e602c87da1ad0b8dc32
watthanai_c <notovel2@gmail.com>
2019-11-17 22:33:02 +0700

    fix line bot router

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const scgRouter = require('./routers/scg-router')
 const placesGoogleSearchRouter = require('./routers/places-google-search-router')
 const botRouter = require('./routers/line-bot-router')
 const cors = require('cors')
+const {lineConfig} = require('./config')
+const {middleware} = require('@line/bot-sdk')
+
 app.use(cors({
     origin: origin
 }))
+app.use(middleware(lineConfig))
 app.use(express.json())
 app.use('/SCG', scgRouter)
 app.use('/places', placesGoogleSearchRouter)
--- routers/line-bot-router.js
+++ routers/line-bot-router.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {middleware} = require('@line/bot-sdk')
-const {lineConfig} = require('../config')
 
 router.get('/', (req, res) => {
     res.send('GET')
 })
 
-router.post('/', middleware(lineConfig), (req, res) => {
+router.post('/', (req, res) => {
     console.log('req', req);
     res.json({})
     // res.json(req.body.events)
